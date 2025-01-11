03a3ced20f7111395eec38f06de71c63938cd99d
Matheus Bassi <bassi.matheus@outlook.com>
2025-01-11 00:38:06 -0300

    feat: Configuração inicial do RabbitMQ

---
+++ src/main/java/bassi/itau_unibanco/exerc3_itau_unibanco/infra/rabbitmq/RabbitMqConfiguration.java
@@ -0,0 +1,23 @@
+package bassi.itau_unibanco.exerc3_itau_unibanco.infra.rabbitmq;
+
+import org.springframework.amqp.core.Declarable;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqConfiguration {
+
+    public static final String CADASTRO_PRODUTO_QUEUE = "itau-unibanco-cadastro-produto";
+
+    @Bean
+    public Declarable produtoCadastradoQueue() {
+        return new Queue(CADASTRO_PRODUTO_QUEUE);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+}
