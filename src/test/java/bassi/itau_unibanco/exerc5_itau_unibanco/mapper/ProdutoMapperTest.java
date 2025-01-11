ec7f66c3391f4bc4829c5d86f20c85f79a292aae
Matheus Bassi <bassi.matheus@outlook.com>
2025-01-11 20:31:56 -0300

    test: Atualiza cenários de teste para mapper

--- src/test/java/bassi/itau_unibanco/exerc5_itau_unibanco/mapper/ProdutoMapperTest.java
+++ src/test/java/bassi/itau_unibanco/exerc5_itau_unibanco/mapper/ProdutoMapperTest.java
@@ -15,8 +15,7 @@ import org.springframework.test.context.ActiveProfiles;
 import java.math.BigDecimal;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 @Epic("Mapeamento de Objetos")
 @Feature("Mapeamento para ProdutoEntity")
@@ -26,6 +25,36 @@ class ProdutoMapperTest {
 
     @Autowired
     private ProdutoMapper mapper;
+
+    @Test
+    @Story("Mapeia ProdutoRequest para ProdutoEntity")
+    @Description("Valida se o método mapToProdutoEntity atualiza corretamente um ProdutoEntity existente com informações de um ProdutoRequest.")
+    @DisplayName("Deve mapear ProdutoRequest para ProdutoEntity.")
+    void mapeiaProdutoRequestParaProdutoEntity_DeveMapearCorretamente() {
+        var produtoRequest = ProdutoStub.buildProdutoRequest("Cartão PJ", BigDecimal.valueOf(50.00), "PJ");
+        var produtoEntity = Assertions.assertDoesNotThrow(() -> this.mapper.mapToProdutoEntity(produtoRequest));
+
+        assertNotNull(produtoEntity);
+        assertNull(produtoEntity.getId());
+        assertEquals("Cartão PJ", produtoEntity.getNome());
+        assertEquals(BigDecimal.valueOf(50.00), produtoEntity.getPreco());
+        assertEquals("PJ", produtoEntity.getCategoria());
+    }
+
+    @Test
+    @Story("Mapeia ProdutoEntity para ProdutoResponse")
+    @Description("Valida se o método mapToProdutoEntity atualiza corretamente um ProdutoEntity existente com informações de um ProdutoRequest.")
+    @DisplayName("Deve mapear ProdutoEntity para ProdutoResponse.")
+    void mapeiaProdutoEntityParaProdutoResponse_DeveMapearCorretamente() {
+        var produtoEntity = ProdutoStub.buildProdutoEntity(UUID.fromString("850db7cf-d747-4dff-8041-426885842420"), "Cartão PJ", BigDecimal.valueOf(25.00), "PJ");
+        var produtoResponse = Assertions.assertDoesNotThrow(() -> this.mapper.mapToProdutoResponse(produtoEntity));
+
+        assertNotNull(produtoResponse);
+        assertEquals("850db7cf-d747-4dff-8041-426885842420", produtoResponse.id().toString());
+        assertEquals("Cartão PJ", produtoResponse.nome());
+        assertEquals(BigDecimal.valueOf(50.00), produtoResponse.preco());
+        assertEquals("PJ", produtoResponse.categoria());
+    }
 
     @Test
     @Story("Atualizar ProdutoEntity com ProdutoRequest")
