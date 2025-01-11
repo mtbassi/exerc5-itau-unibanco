9c2d50d398341dd5e2c2722ff2def3745aeff016
Matheus Bassi <bassi.matheus@outlook.com>
2025-01-10 21:43:29 -0300

    Commit inicial

---
+++ src/test/java/bassi/itau_unibanco/exerc1_itau_unibanco/Exerc1ItauUnibancoApplicationTests.java
@@ -0,0 +1,13 @@
+package bassi.itau_unibanco.exerc1_itau_unibanco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exerc1ItauUnibancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
